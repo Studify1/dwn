98ae3b39afcfcb9eb030ae9547fd9041c43cbbfb
MadaIsaac <madaisaac2002@gmail.com>
2026-01-03 12:57:36 +0300

    adsd

--- pythonfile.py
+++ pythonfile.py
@@ -1,1 +1,9 @@
-print("hell word")
+from pyscript import display, Element, when
+
+# Update a specific HTML element
+Element("output").write("Python is running!")
+
+# React to button clicks
+@when("click", "#my-button")
+def handle_click(event):
+    display("Button clicked!", target="output")
